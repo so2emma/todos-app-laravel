4ea85e266c9b6444e36bdc4d128d045646a3ffd9
Oso Emmanuel <osoemmanuel1969@gmail.com>
2022-07-30 15:57:27 +0100

    implementing Layouts

--- resources/views/Todos/index.blade.php
+++ resources/views/Todos/index.blade.php
@@ -1,41 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
+@extends("layouts.app")
+@section("title", "Todo List")
+@section("content")
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <h1 class="text-center my-5">Todos Page</h1>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <h1 class="text-center my-5">Todos Page</h1>
+        <div class="card ">
+            <div class="card-header">
+                Todos
+            </div>
 
-                <div class="card ">
-                    <div class="card-header">
-                        Todos
-                    </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    @foreach ($todos as $todo )
+                    <li class="list-group-item">
+                        {{ $todo->name }}
 
-                    <div class="card-body">
-                        <ul class="list-group">
-                            @foreach ($todos as $todo )
-                            <li class="list-group-item">
-                                {{ $todo->name }}
-
-                                <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-end">View</a>
-                            </li>
-                        @endforeach
-                        </ul>
-                    </div>
-                </div>
+                        <a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm float-end">View</a>
+                    </li>
+                @endforeach
+                </ul>
             </div>
         </div>
     </div>
-
-
-
-</body>
-</html>
+</div>
+@endsection
--- resources/views/Todos/show.blade.php
+++ resources/views/Todos/show.blade.php
@@ -1,39 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("layouts.app")
+@section("title", "Todo")
+@section("content")
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show - TODO</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
+<h1 class="text-center my-5">
+    {{ $todo->name }}
+</h1>
 
-<body>
-
-    <div class="container">
-        <h1 class="text-center my-5">
-            {{ $todo->name }}
-        </h1>
-
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header">
-                        Description
-                    </div>
-                    <div class="card-body">
-                        {{ $todo->description }}
-                    </div>
-                </div>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-header">
+                Description
+            </div>
+            <div class="card-body">
+                {{ $todo->description }}
             </div>
         </div>
     </div>
-
-
-
-
-</body>
-
-</html>
+</div>
+@endsection
